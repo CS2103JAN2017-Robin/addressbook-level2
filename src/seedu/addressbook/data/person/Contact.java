f8ce02bd7334b33b9a2ec7cb30264bdf7b3d716d
LOH ZI BIN ROBIN <robinloh@u.nus.edu>
2017-02-12 15:47:46 +0800

    Extract commonalities from Phone, Email and Address classes into a parent class called Contact.

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,33 @@
+package seedu.addressbook.data.person;
+
+/**
+ * Represents a particular contact element from a person.
+ * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
+ */
+public class Contact {
+    
+    public String value;
+    protected boolean isPrivate;
+    
+    public Contact() {
+        super();
+    }
+    
+    @Override
+    public String toString() {
+        return value;
+    }
+    
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+    
+    /*
+     * Returns if the contact element is private.
+     */
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+
+}
