9bbbdaf5436616e3f89363884a977e74a3ab6abe
LOH ZI BIN ROBIN <robinloh@u.nus.edu>
2017-02-27 00:25:43 +0800

    Delete unused import statement

--- src/seedu/addressbook/data/person/Name.java
+++ src/seedu/addressbook/data/person/Name.java
@@ -3,7 +3,6 @@ package seedu.addressbook.data.person;
 import seedu.addressbook.data.exception.IllegalValueException;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
